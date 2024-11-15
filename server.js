12b0b92730f311f7a22be5523908f1c5f4c17bd7
yangjia <1049861657@qq.com>
2024-11-15 08:55:39 +0800

    更改webgl加载方式

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+// 为了调试，添加日志
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+// 服务主页面的静态文件
+app.use(express.static(__dirname));
+
+// WebGL相关文件的服务
+app.use('/Build', express.static(path.join(__dirname, 'packaged_Navigation/Build')));
+app.use('/TemplateData', express.static(path.join(__dirname, 'packaged_Navigation/TemplateData')));
+app.use('/StreamingAssets', express.static(path.join(__dirname, 'packaged_Navigation/StreamingAssets')));
+
+// 所有路由都返回主页面，这样无论访问什么URL都会显示工具箱
+app.get('*', (req, res) => {
+    // 如果是 /webgl 路径，返回 WebGL 页面
+    if (req.path === '/webgl') {
+        res.sendFile(path.join(__dirname, 'packaged_Navigation/index.html'));
+    } else {
+        // 其他所有路径都返回主页面
+        res.sendFile(path.join(__dirname, 'index.html'));
+    }
+});
+
 const port = 8080;
-const ip = '192.168.1.157';  // 替换为您的实际IP地址
-
-// 设置静态文件目录
-app.use(express.static('./'));
-
-// 启动服务器
-app.listen(port, ip, () => {
-    console.log(`服务器运行在 http://${ip}:${port}`);
+app.listen(port, () => {
+    console.log(`服务器运行在 http://localhost:${port}`);
+    console.log(`WebGL页面在 http://localhost:${port}/webgl`);
 }); 
